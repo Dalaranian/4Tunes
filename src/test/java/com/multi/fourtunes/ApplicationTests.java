5961be5351e8f56f01a02a08f9371846e33e1e30
JMBAE <bejm1997@gmail.com>
2023-05-22 13:16:59 +0900

    init commit

---
+++ src/test/java/com/multi/fourtunes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.fourtunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
