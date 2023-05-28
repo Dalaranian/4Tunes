31d5866894d9ee8268a816cc9f1338e27e7460a9
J.M.Bae <bejm1997@gmail.com>
2023-05-28 18:22:24 +0900

    마이페이지 페이징 완료

--- src/main/java/com/multi/fourtunes/model/controller/paging/InnerPagingController.java
+++ src/main/java/com/multi/fourtunes/model/controller/paging/InnerPagingController.java
@@ -8,13 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/innerpaging")
 public class InnerPagingController {
 
+	// 로그인 페이지
+	
+	// 회원가입 페이지로 전환
 	@GetMapping("/login/join")
-	public String gotoJoin() {
+	public String gotoLoginJoin() {
 		return "login_join";
 	}
 
-	@RequestMapping("/mypage")
-	public class MyPage {
-
+	// 마이페이지
+	
+	// 내 정보 보기로 전환
+	@GetMapping("/mypage/user")
+	public String gotoMyPageUser() {
+		return "mypage_user";
+	}
+	
+	@GetMapping("/mypage/community")
+	public String gotoMyPageCommmunity() {
+		return "mypage_community";
 	}
 }
