db819ba7ac42d0a1d9700071735a6f74ccf7d7f6
J.M.Bae <bejm1997@gmail.com>
2023-05-28 19:17:43 +0900

    Update InnerPagingController.java
    임시 컨트롤러 작업

--- src/main/java/com/multi/fourtunes/model/controller/paging/InnerPagingController.java
+++ src/main/java/com/multi/fourtunes/model/controller/paging/InnerPagingController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// 프론트 작성의 편의를 위한 임시 페이징 클래스입니다. 
+
 @Controller
 @RequestMapping("/innerpaging")
 public class InnerPagingController {
@@ -28,5 +30,38 @@ public class InnerPagingController {
 	@GetMapping("/mypage/community")
 	public String gotoMyPageCommmunity() {
 		return "mypage_community";
+	}
+	
+	// 커뮤니티
+	
+	// 글 작성 페이지로 이동
+	@GetMapping("/community/write")
+	public String gotoCommunityWrite() {
+		return "community_write";
+	}
+	
+	@GetMapping("/community/detail")
+	public String gotoCommunityDetail() {
+		return "community_detail";
+	}
+	
+	// 메인페이지 재생목록
+	
+	// 내 재생목록으로 이동
+	@GetMapping("/playlist/mine")
+	public String gotoMyPlayList() {
+		return "playlist_mine";
+	}
+	
+	// public 재생목록으로 이동
+	@GetMapping("/playlist/public")
+	public String gotoPlaylistPublic() {
+		return "playlist_public";
+	}
+	
+	// 오늘의 선곡으로 이동
+	@GetMapping("/playlist/suggested")
+	public String gotoPlayListSuggested() {
+		return "playlist_todaypick";
 	}
 }
