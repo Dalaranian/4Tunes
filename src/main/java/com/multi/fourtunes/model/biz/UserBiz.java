ac387a68cd18d6d2d6b34aa90d5e0d864784f05b
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-05-31 14:58:38 +0900

    Revert "Yoon a general login"

--- src/main/java/com/multi/fourtunes/model/biz/UserBiz.java
+++
@@ -1,9 +0,0 @@
-package com.multi.fourtunes.model.biz;
-
-import com.multi.fourtunes.model.dto.UserDto;
-
-public interface UserBiz {
-
-	public UserDto login(UserDto dto);
-	
-}
--- src/main/java/com/multi/fourtunes/model/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.multi.fourtunes.model.dao;
-
-import com.multi.fourtunes.model.dto.UserDto;
-
-public interface UserDao {
-
-	public UserDto login(UserDto dto);
-	
-}
