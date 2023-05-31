66f5e31953d2675bd5efdc3b74a2699cd69e36b5
Park Yoon A <pya7106@naver.com>
2023-05-31 14:18:37 +0900

    General Login
    일반 로그인 완료

---
+++ src/main/java/com/multi/fourtunes/model/biz/UserBiz.java
@@ -0,0 +1,9 @@
+package com.multi.fourtunes.model.biz;
+
+import com.multi.fourtunes.model.dto.UserDto;
+
+public interface UserBiz {
+
+	public UserDto login(UserDto dto);
+	
+}
---
+++ src/main/java/com/multi/fourtunes/model/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.multi.fourtunes.model.dao;
+
+import com.multi.fourtunes.model.dto.UserDto;
+
+public interface UserDao {
+
+	public UserDto login(UserDto dto);
+	
+}
