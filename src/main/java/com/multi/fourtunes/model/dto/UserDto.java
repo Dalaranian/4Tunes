a71b6d0f64eebd4e8b15ae45950a2a4a9db91e9d
Park Yoon A <pya7106@naver.com>
2023-05-31 12:30:59 +0900

    Create UserDto.java
    lombok 사용하여 UserDto 생성

---
+++ src/main/java/com/multi/fourtunes/model/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.multi.fourtunes.model.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+	
+	private int user_no;
+	private String user_id;
+	private String user_name;
+	private String user_pw;
+	private String user_grade;
+	private int membership_duration;
+	private Date pay_date;
+	
+	
+}
