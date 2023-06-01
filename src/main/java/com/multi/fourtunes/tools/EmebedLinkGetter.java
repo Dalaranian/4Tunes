4e3206aa5b72e70a47483c420f6be5cbda7a65ed
J.M.Bae <bejm1997@gmail.com>
2023-06-01 18:12:41 +0900

    Create EmebedLinkGetter.java
    살려주세요...

---
+++ src/main/java/com/multi/fourtunes/tools/EmebedLinkGetter.java
@@ -0,0 +1,51 @@
+package com.multi.fourtunes.tools;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class EmebedLinkGetter {
+
+
+	WebDriver webdriver;
+	
+	/**
+	 * @param String title 노래 제목
+	 * @param String link ManiaDb 링크
+	 * @return
+	 */
+	public String getLink(String title, String link) {
+		webdriver = null;
+		File webDriverFile = new File("WebDriver/chromedriver_mac64/chromedriver.exec");
+		
+		System.out.println(webDriverFile.getName() + " " + webDriverFile.getAbsolutePath());
+		
+//		try {
+//	        // Chrome 웹 드라이버 경로 설정
+//			String driverPath = webDriverFile.getAbsolutePath();
+//			System.setProperty("webdriver.chrome.driver", driverPath);
+//			
+//	        // Chrome 옵션 설정
+//	        ChromeOptions options = new ChromeOptions();
+//	        options.addArguments("--headless"); // 브라우저 창을 띄우지 않고 실행
+//
+//	        // Chrome 웹 드라이버 인스턴스 생성
+//	        WebDriver driver = new ChromeDriver(options);
+//	        
+//	        try {
+//				driver.get(link);
+//				
+//			} catch (Exception e) {
+//				// TODO: handle exception
+//			}
+//
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}
+		
+		return "null";
+	}
+}
