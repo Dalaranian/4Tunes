eaa744a8daf1419da1ce2186c1e8ad02da5c0b69
Yujihoon1 <yjh150062@naver.com>
2023-06-02 16:10:50 +0900

    login_join
    login_join

--- src/main/resources/static/js/sociallogin_naver.js
+++ src/main/resources/static/js/sociallogin_naver.js
@@ -8,3 +8,21 @@ naver_id_login.setDomain("http://localhost:8787");
 naver_id_login.setState(state);
 naver_id_login.setPopup(false);
 naver_id_login.init_naver_id_login();
+
+
+var naver_id_login = new naver_id_login(
+        "_Bz0QIIPqcH1PJ3NCi5I",
+        "http://localhost:8787/nav/login"
+      );
+   	  // 접근 토큰 값 출력
+      alert(naver_id_login.oauthParams.access_token);
+      // 네이버 사용자 프로필 조회
+      naver_id_login.get_naver_userprofile("naverSignInCallback()");
+      // 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
+      function naverSignInCallback() {
+        alert(naver_id_login.getProfileData("email"));
+        alert(naver_id_login.getProfileData("nickname"));
+        alert(naver_id_login.getProfileData("age"));
+      }
+      
+      naver_id_login.get_naver_userprofile("naverSignInCallback()");
