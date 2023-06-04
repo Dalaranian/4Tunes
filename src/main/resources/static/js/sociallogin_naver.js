8e00fc6a028114b7c20eb534022ea3ed3be390c6
Yujihoon1 <yjh150062@naver.com>
2023-06-04 16:36:20 +0900

    jihoon_naver
    naver

--- src/main/resources/static/js/sociallogin_naver.js
+++ src/main/resources/static/js/sociallogin_naver.js
@@ -6,4 +6,4 @@ var state = naver_id_login.getUniqState();
 naver_id_login.setButton("green", 1, 72);
 naver_id_login.setDomain("http://localhost:8787");
 naver_id_login.setState(state);
-naver_id_login.init_naver_id_login("http://localhost:8787/login/sociallogin");
+naver_id_login.init_naver_id_login();
