25e52730f1cff477b31646832cc0182186e0b2c5
JMBAE <bejm1997@gmail.com>
2023-06-07 09:48:58 +0900

    Update LoginController.java
    임포트 안된거 추가

--- src/main/java/com/multi/fourtunes/model/controller/LoginController.java
+++ src/main/java/com/multi/fourtunes/model/controller/LoginController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.multi.fourtunes.model.biz.FaceloginBiz;
 import com.multi.fourtunes.model.biz.LoginBiz;
 import com.multi.fourtunes.model.biz.UserBiz;
 import com.multi.fourtunes.model.dto.UserDto;
