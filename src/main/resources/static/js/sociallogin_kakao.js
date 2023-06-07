7afeac067937c3d0550d561959c8f9cf6e65b3d4
JoMinSu <alstn2944@nate.com>
2023-06-07 13:32:27 +0900

    kakao
    kakao

--- src/main/resources/static/js/sociallogin_kakao.js
+++ src/main/resources/static/js/sociallogin_kakao.js
@@ -1,7 +1,8 @@
 Kakao.init('aef00a860346906c88050b79c5211154'); //발급받은 키 중 javascript키를 사용해준다.
 console.log(Kakao.isInitialized()); // sdk초기화여부판단
 //카카오로그인
-function kakaoLogin() {
+var kakao_id_login = new kakao_id_login
+function kakao_id_login() {
     Kakao.Auth.login({
       success: function (response) {
         Kakao.API.request({
