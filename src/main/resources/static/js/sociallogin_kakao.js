93ed2ff4c23a21f2d742c7eabdf93c3eee8c7b7d
JoMinSu <alstn2944@nate.com>
2023-06-07 17:20:43 +0900

    kakao로그인

--- src/main/resources/static/js/sociallogin_kakao.js
+++ src/main/resources/static/js/sociallogin_kakao.js
@@ -1,16 +1,13 @@
-window.kakaoAsyncInit function(){
-	Kakao.init ('aef00a860346906c88050b79c5211154');
-	document.getElementById('kakao_id_login').onclick = kakao_id_login;
-	console.log(kakao_id_login);
-}
+Kakao.init('aef00a860346906c88050b79c5211154'); //발급받은 키 중 javascript키를 사용해준다.
+console.log(Kakao.isInitialized()); // sdk초기화여부판단
 //카카오로그인
 function kakao_id_login() {
-    window.Kakao.Auth.login({
+    Kakao.Auth.login({
       success: function (response) {
-      console.log(response);
-        window.Kakao.API.request({
+        Kakao.API.request({
           url: '/v2/user/me',
-          success: function (response) {
+       
+       success: function (response) {
         	  console.log(response)
           },
           fail: function (error) {
@@ -23,4 +20,3 @@ function kakao_id_login() {
       },
     })
   }
-  
