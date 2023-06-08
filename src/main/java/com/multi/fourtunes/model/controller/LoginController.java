b3193bd8afc0613c20b26e044689bba45d930ee5
JoMinSu <alstn2944@nate.com>
2023-06-08 15:20:11 +0900

    kakao Controllor

--- src/main/java/com/multi/fourtunes/model/controller/LoginController.java
+++ src/main/java/com/multi/fourtunes/model/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.multi.fourtunes.model.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -9,8 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.multi.fourtunes.model.biz.FaceloginBiz;
 import com.multi.fourtunes.model.biz.LoginBiz;
@@ -72,6 +76,34 @@ public class LoginController {
 	@GetMapping("/callback")
 	public String callback() {
 		return "callback";
+	}
+	
+	@PostMapping("/kakao")
+	public String kakao(@RequestParam("email") String email, @RequestParam("name") String name, Model model,HttpSession session){
+		System.out.println(email + " " + name);
+		boolean isUserExist = loginBiz.checkUserExist(email, name);
+		if (isUserExist) {
+			// DB에 있는 이메일이면 로그인 진행
+			// 로그인 처리 로직 구현
+
+			UserDto loginUser = new UserDto();
+			loginUser.setUser_id(email);
+			loginUser.setUser_name(name);
+
+			UserDto res = loginBiz.socialLogin(loginUser);
+			session.setAttribute("login", res);
+
+			return "index"; // 로그인 후 이동할 페이지
+		} else {
+			// DB에 없는 이메일이면 ID 입력칸과 이름 입력칸으로 이동
+			// 회원 가입 페이지로 이동
+			model.addAttribute("email", email);
+			model.addAttribute("name", name);
+
+			String[] keywordList = loginBiz.getKeyword();
+			model.addAttribute("keywordlist", keywordList);
+			return "login_socialjoin";
+		}
 	}
 
 	@PostMapping("/login")
