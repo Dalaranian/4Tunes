a32b5af6eb10e4c82ebbcad2d7c1b7b1aa7a3b58
J.M.Bae <bejm1997@gmail.com>
2023-06-08 18:18:13 +0900

    Refactoring YoutubeApi

--- src/main/java/com/multi/fourtunes/apis/YoutubeApi.java
+++ src/main/java/com/multi/fourtunes/apis/YoutubeApi.java
@@ -27,8 +27,8 @@ public class YoutubeApi {
 
 	// 요청할 API 의 URL
 	private final String apiUrl = "https://www.googleapis.com/youtube/v3/search";
-	
-	private final static String EMBED_LINK_PREFIX = "https://www.youtube.com/embed/"; 
+
+	private final static String EMBED_LINK_PREFIX = "https://www.youtube.com/embed/";
 
 	// application.properties 에 저장되어있는 값 불러오기
 	@Value("${youtubeapi.key}")
@@ -37,30 +37,33 @@ public class YoutubeApi {
 	public String embedLinkGetter(String singer, String title) {
 
 		restTemplate = new RestTemplate();
-		
-//		파라미터로 넘어온 단어들 조합하기
-		StringBuilder query = new StringBuilder();
-		query.append(singer);
-		query.append("-");
-		query.append(title);
-		
-		System.out.println(query.toString());
+
+		String query = urlBuilder(singer, title);
 
 		// UriComponentsBuilder를 사용하여 URL 및 쿼리 파라미터를 생성
 		UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(apiUrl).queryParam("part", "id,snippet")
-				.queryParam("q", query.toString()).queryParam("key", apiKey).queryParam("type", "video").build();
+				.queryParam("q", query).queryParam("key", apiKey).queryParam("type", "video").build();
 
 		ResponseEntity<String> responseEntity = restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, null,
 				String.class);
 
 		JSONObject response = new JSONObject(responseEntity.getBody());
-
 		ObjectMapper objMapper = new ObjectMapper();
 
-		
-		// Json 을 탐색해서 videoId를 id 에 저장함
+		String id = jsonParser(response, objMapper);
+
+		return EMBED_LINK_PREFIX + id;
+	}
+
+	/**
+	 * @param response
+	 * @param objMapper
+	 * @return response 를 파싱해서, embedLink 를 반환
+	 */
+	private String jsonParser(JSONObject response, ObjectMapper objMapper) {
+		JsonNode rootNode;
 		try {
-			JsonNode rootNode = objMapper.readTree(response.toString());
+			rootNode = objMapper.readTree(response.toString());
 //			System.out.println(rootNode.toPrettyString());
 //			System.out.println("-------------------------------------");
 			JsonNode itemsNode = rootNode.get("items");
@@ -73,17 +76,29 @@ public class YoutubeApi {
 //			System.out.println(targetIdNode.toPrettyString());
 //			System.out.println("-------------------------------------");
 			String id = targetIdNode.get("videoId").asText();
-
-			return EMBED_LINK_PREFIX + id;
+			
+			return id;
 		} catch (JsonMappingException e) {
 			// TODO Auto-generated catch block
-			System.out.println(singer + " " + title + "노래는 뭔가 뭔가 이상함");
+			e.printStackTrace();
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
-			System.out.println(singer + " " + title + "노래는 뭔가 뭔가 이상함");
+			e.printStackTrace();
 		}
-
 		return "undefine";
+	}
+
+	/**
+	 * @param singer
+	 * @param title
+	 * @return 위 파라미터로 조합된 URL
+	 */
+	private String urlBuilder(String singer, String title) {
+		StringBuilder query = new StringBuilder();
+		query.append(singer);
+		query.append("-");
+		query.append(title);
+		return query.toString();
 	}
 
 }
