9a1a00b0dfd7346921605b7988865ffad96023eb
JoMinSu <alstn2944@nate.com>
2023-06-08 11:33:29 +0900

    js 수정

--- src/main/resources/static/js/sociallogin_kakao.js
+++ src/main/resources/static/js/sociallogin_kakao.js
@@ -6,16 +6,25 @@ function kakao_id_login() {
       success: function (response) {
         Kakao.API.request({
           url: '/v2/user/me',
-       
-       success: function (response) {
-        	  console.log(response)
-          },
+          success: function(response) {
+					//console.log("성공resopnse");
+					//console.log(response)
+					/*const email = response.kakao_account.email;*/
+					const kakao_name = response.properties.nickname;
+					const kakao_email = (response.kakao_account.kakao_email != undefined ? response.kakao_account.kakao_email : '');
+					console.log(name); 
+					console.log(email);
+					window.location.href = "/login/sociallogin?email=" + encodeURIComponent(kakao_email) + "&name=" + encodeURIComponent(kakao_name);
+	
+				},
           fail: function (error) {
+          alert('로그인해야 이용 가능한 기능입니다.');
             console.log(error)
           },
         })
       },
       fail: function (error) {
+      alert('zkzkdh에 로그인해야 이용 가능한 기능입니다.');
         console.log(error)
       },
     })
