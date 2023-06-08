1a7b2143412fe125b8b9462f0bd3ec35f9230b74
JoMinSu <alstn2944@nate.com>
2023-06-08 15:20:27 +0900

    kakao js 수정

--- src/main/resources/static/js/sociallogin_kakao.js
+++ src/main/resources/static/js/sociallogin_kakao.js
@@ -1,31 +1,43 @@
-Kakao.init('aef00a860346906c88050b79c5211154'); //발급받은 키 중 javascript키를 사용해준다.
-console.log(Kakao.isInitialized()); // sdk초기화여부판단
-//카카오로그인
-function kakao_id_login() {
-    Kakao.Auth.login({
-      success: function (response) {
-        Kakao.API.request({
-          url: '/v2/user/me',
-          success: function(response) {
-					//console.log("성공resopnse");
-					//console.log(response)
-					/*const email = response.kakao_account.email;*/
-					const kakao_name = response.properties.nickname;
-					const kakao_email = (response.kakao_account.kakao_email != undefined ? response.kakao_account.kakao_email : '');
-					console.log(name); 
-					console.log(email);
-					window.location.href = "/login/sociallogin?email=" + encodeURIComponent(kakao_email) + "&name=" + encodeURIComponent(kakao_name);
-	
-				},
-          fail: function (error) {
-          alert('로그인해야 이용 가능한 기능입니다.');
-            console.log(error)
-          },
-        })
-      },
-      fail: function (error) {
-      alert('zkzkdh에 로그인해야 이용 가능한 기능입니다.');
-        console.log(error)
-      },
-    })
-  }
+  window.Kakao.init("aef00a860346906c88050b79c5211154");
+	function kakao_id_login(){
+	    window.Kakao.Auth.login({
+	        scope   : 'profile_nickname, account_email',
+	        success : function(authObj){
+	            console.log(authObj);
+	            window.Kakao.API.request({
+	                url     : '/v2/user/me', 
+	                success : res => {
+	                    const kakao_account = res.kakao_account;
+	                    console.log(kakao_account);
+	                    console.log(kakao_account.profile.nickname);
+ 	                    let obj = {
+	                    		userId : kakao_account.email,
+	                    		userName : kakao_account.profile.nickname
+	                    };
+	                    
+ 	                   const json_kakao_account = JSON.stringify(obj);
+	                   /* 불러온 계정 정보 컨트롤러로 보내기 */
+	                     $.ajax({
+	                    	 url: '/login/kakao',
+		                        type: 'POST',
+		                        data: json_kakao_account,
+		                        contentType: 'application/json; charset=utf-8',
+		                        dataType: 'json',
+		                        success: function(res) {
+		                   			if(res.result === "true"){
+		                   				alert("로그인 성공")
+		                   				location.href="/index";
+		                   			}else{
+		                   				alert(res.userName+"님은 최초로 로그인 하셨음으로, 추가 정보가 필요합니다. ");
+		                   				location.href="/kakaoAuth/kakaoJoin?userId="+res.userId+"&userName="+res.userName;
+		                   			}
+		                        },
+		                        error: function(error) {
+		                            console.log(error);
+		                        }
+	                    });
+	                }
+	            });
+	        }
+	    });
+	}
