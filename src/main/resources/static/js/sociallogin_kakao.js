ead40f09420b1ce4840e9b5196d9fb3ba41869a7
JoMinSu <alstn2944@nate.com>
2023-06-09 11:43:30 +0900

    kakao Post 오류

--- src/main/resources/static/js/sociallogin_kakao.js
+++ src/main/resources/static/js/sociallogin_kakao.js
@@ -1,3 +1,4 @@
+
   window.Kakao.init("aef00a860346906c88050b79c5211154");
 	function kakao_id_login(){
 	    window.Kakao.Auth.login({
@@ -10,12 +11,13 @@
 	                    const kakao_account = res.kakao_account;
 	                    console.log(kakao_account);
 	                    console.log(kakao_account.profile.nickname);
- 	                    let obj = {
+	                    
+	                     let obj = {
 	                    		userId : kakao_account.email,
 	                    		userName : kakao_account.profile.nickname
 	                    };
-	                    
- 	                   const json_kakao_account = JSON.stringify(obj);
+						
+	                    const json_kakao_account = JSON.stringify(obj);
 	                   /* 불러온 계정 정보 컨트롤러로 보내기 */
 	                     $.ajax({
 	                    	 url: '/login/kakao',
@@ -29,7 +31,7 @@
 		                   				location.href="/index";
 		                   			}else{
 		                   				alert(res.userName+"님은 최초로 로그인 하셨음으로, 추가 정보가 필요합니다. ");
-		                   				location.href="/kakaoAuth/kakaoJoin?userId="+res.userId+"&userName="+res.userName;
+		                   				
 		                   			}
 		                        },
 		                        error: function(error) {
