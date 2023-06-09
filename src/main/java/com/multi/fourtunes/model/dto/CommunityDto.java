b4204d5127854f3986a92bc2d755c27e6559fc05
J.M.Bae <bejm1997@gmail.com>
2023-06-09 11:51:01 +0900

    Mapper수정
    후 DTO 잘 받아와짐

--- src/main/java/com/multi/fourtunes/model/dto/CommunityDto.java
+++ src/main/java/com/multi/fourtunes/model/dto/CommunityDto.java
@@ -15,11 +15,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class CommunityDto {
-	private int BOARD_NO;
-	private String BOARD_TITLE	;
-	private String BOARD_CONTENT;
-	private int USER_NO;
-	private int BOARD_REPORT_CNT;
-	private Date BOARD_WRITE_DATE;
-	private int BOARD_VIEW_CNT;
+    private int boardNo;
+    private String boardTitle;
+    private String boardContent;
+    private int userNo;
+    private int boardReportCnt;
+    private Date boardWriteDate;
+    private int boardViewCnt;
 }
--- src/main/java/com/multi/fourtunes/model/mapper/CommunityMapper.java
+++ src/main/java/com/multi/fourtunes/model/mapper/CommunityMapper.java
@@ -3,6 +3,8 @@ package com.multi.fourtunes.model.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.multi.fourtunes.model.dto.CommunityDto;
@@ -10,11 +12,18 @@ import com.multi.fourtunes.model.dto.CommunityDto;
 @Mapper
 public interface CommunityMapper {
 
-    @Select(" SELECT * FROM COMMUNITY_BOARD ")
-    List<CommunityDto> getAll();
-    
-    @Select(" SELECT * FROM COMMUNITY_BOARD WHERE boardNo = #{boardNo} ")
-    CommunityDto get(int boardNo);
+	@Results({ @Result(property = "boardNo", column = "BOARD_NO"),
+			@Result(property = "boardTitle", column = "BOARD_TITLE"),
+			@Result(property = "boardContent", column = "BOARD_CONTENT"),
+			@Result(property = "userNo", column = "USER_NO"),
+			@Result(property = "boardReportCnt", column = "BOARD_REPORT_CNT"),
+			@Result(property = "boardWriteDate", column = "BOARD_WRITE_DATE"),
+			@Result(property = "boardViewCnt", column = "BOARD_VIEW_CNT") })
+	@Select("SELECT * FROM COMMUNITY_BOARD")
+	List<CommunityDto> getAll();
+
+	@Select(" SELECT * FROM COMMUNITY_BOARD WHERE boardNo = #{boardNo} ")
+	CommunityDto get(int boardNo);
 
 	int insert(CommunityDto community);
 
