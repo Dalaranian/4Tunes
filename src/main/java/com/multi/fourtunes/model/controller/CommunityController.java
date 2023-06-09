2137c1c5c84a5b09c996bbe3a9c118ce34d8cc50
Yunji97 <simyg97@naver.com>
2023-06-09 11:37:05 +0900

    Dto 변경
    이름 대문자로?

--- src/main/java/com/multi/fourtunes/model/controller/CommunityController.java
+++ src/main/java/com/multi/fourtunes/model/controller/CommunityController.java
@@ -1,11 +1,8 @@
 package com.multi.fourtunes.model.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
