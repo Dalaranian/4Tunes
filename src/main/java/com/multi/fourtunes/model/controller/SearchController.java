d50eeb78dd2ec4bf60f94203863378e126602416
J.M.Bae <bejm1997@gmail.com>
2023-06-10 21:16:31 +0900

    임시 검색 기능 구현
    임시 검색 컨트롤러 및, 테스트용 페이지 구현 완료

---
+++ src/main/java/com/multi/fourtunes/model/controller/SearchController.java
@@ -0,0 +1,42 @@
+package com.multi.fourtunes.model.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.fourtunes.apis.ManiaDbApi;
+import com.multi.fourtunes.apis.YoutubeApi;
+import com.multi.fourtunes.model.dto.SongDto;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+	@Autowired
+	ManiaDbApi mania;
+
+	@Autowired
+	YoutubeApi youtube;
+
+	@GetMapping("/query")
+	public String search(String query, Model model) {
+//		System.out.println(query);
+
+		// 검색 로직
+		mania.setPrompt(query);
+		mania.setType(false);
+		ArrayList<SongDto> result = mania.search();
+		for (SongDto currentSong : result) {
+			currentSong.setSongLink(youtube.embedLinkGetter(currentSong.getSongArtist(), currentSong.getSongTitle()));
+			System.out.println(currentSong);
+		}
+		
+		model.addAttribute("searchresult", result);
+
+		return "playlist_searchresult"; // 결과를 표시할 View의 이름을 반환
+	}
+}
