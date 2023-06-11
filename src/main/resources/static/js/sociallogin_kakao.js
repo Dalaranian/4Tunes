6bc8bb18cd844eed30df2b5999319f869fa2e453
JoMinSu <alstn2944@nate.com>
2023-06-11 15:17:06 +0900

    카카오 소셜 구현 완

--- src/main/resources/static/js/sociallogin_kakao.js
+++ src/main/resources/static/js/sociallogin_kakao.js
@@ -1,22 +1,30 @@
-window.Kakao.init("aef00a860346906c88050b79c5211154");
-document.getElementById('kakao_id_login').onclick = kakao_id_login;
-function kakao_id_login(){
-	   	window.Kakao.Auth.login({
-	        success : function(response){
-	            console.log(response);
-	            window.Kakao.API.request({
-	                url     : '/v2/user/me', 
-	                success : function(response) {
-	               		console.log(response);
-	                    const kakao_account = kakao_account;
-	                    const userId : kakao_account.email,
-	                    const userName : kakao_account.profile.name
-	                    console.log(kakao_account);
-	                    console.log(kakao_account.profile.name);
-	                    window.location.href = "/login/sociallogin?email=" + encodeURIComponent(userId) + 
-	                    						"&name=" + encodeURIComponent(userName);
-	                }
-	            });
-	        }
-	    });
-	}
+Kakao.init('aef00a860346906c88050b79c5211154'); //발급받은 키 중 javascript키를 사용해준다.
+// fbLogin 함수 정의
+function kakao_id_login() {
+    Kakao.Auth.login({
+      success: function (response) {
+        Kakao.API.request({
+          url: '/v2/user/me',
+          success: res => {
+          		const kakao_account = res.kakao_account
+          		const kakao_name = kakao_account.profile.nickname;
+				const kakao_email = kakao_account.email;
+                window.location.href = "/login/sociallogin?email=" + encodeURIComponent(kakao_email) + 
+                										"&name=" + encodeURIComponent(kakao_name);
+                console.log(kakao_name);
+                console.log(akako_email);
+                },
+		  fail: function (error) {
+		          alert('로그인해야 이용 가능한 기능입니다.');
+		          console.log(error)
+         		 },
+         		 })
+         		 },
+		  fail: function (error) {
+		      	alert('카카오에 로그인해야 이용 가능한 기능입니다.');
+		        console.log(error)
+			   },
+	      })
+  }
+
+  
