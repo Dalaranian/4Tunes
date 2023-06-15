4160e38ee844c1d795cade127ccb77bc97a25fd6
J.M.Bae <bejm1997@gmail.com>
2023-06-15 13:44:07 +0900

    Update SearchController.java
    Rest Controller 로 변경

--- src/main/java/com/multi/fourtunes/model/controller/SearchController.java
+++ src/main/java/com/multi/fourtunes/model/controller/SearchController.java
@@ -2,42 +2,52 @@ package com.multi.fourtunes.model.controller;
 
 import java.util.ArrayList;
 
+import com.multi.fourtunes.model.apis.ManiaDbApi;
+import com.multi.fourtunes.model.apis.YoutubeApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.multi.fourtunes.apis.ManiaDbApi;
-import com.multi.fourtunes.apis.YoutubeApi;
 import com.multi.fourtunes.model.dto.SongDto;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/search")
 public class SearchController {
 
 	@Autowired
-	ManiaDbApi mania;
-
-	@Autowired
 	YoutubeApi youtube;
 
+	@Autowired
+	ManiaDbApi mania;
+
 	@GetMapping("/query")
-	public String search(String query, Model model) {
+	public ModelAndView search(String query) {
 //		System.out.println(query);
+
+		ModelAndView modelAndView = new ModelAndView();
 
 		// 검색 로직
 		mania.setPrompt(query);
 		mania.setType(false);
 		ArrayList<SongDto> result = mania.search();
-		result.stream().forEach(currentSong -> currentSong
-				.setSongLink(youtube.embedLinkGetter(currentSong.getSongArtist(), currentSong.getSongTitle())));
-		for(SongDto dto:result) {
-			System.out.println(dto);
-		}
-		model.addAttribute("qeury", query);
-		model.addAttribute("searchresult", result);
+//		result.stream().forEach(currentSong -> currentSong
+//				.setSongLink(youtube.embedLinkGetter(currentSong.getSongArtist(), currentSong.getSongTitle())));
 
-		return "playlist_searchresult"; // 결과를 표시할 View의 이름을 반환
+		// test 를 위해 더미데이터 넣기
+		for(SongDto dto:result) {
+			dto.setSongLink(youtube.testLinkGetter());
+		}
+
+		modelAndView.addObject("query", query);
+		modelAndView.addObject("searchresult", result);
+		modelAndView.setViewName("playlist_searchresult");
+//		model.addAttribute("query", query);
+//		model.addAttribute("searchresult", result);
+
+		return modelAndView;
 	}
 }
