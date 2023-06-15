c8aa3e25ca30c114e898ba2669c122d737da6bd6
J.M.Bae <bejm1997@gmail.com>
2023-06-15 17:05:27 +0900

    Update PlaylistBizImple.java

--- src/main/java/com/multi/fourtunes/model/biz/PlaylistBizImple.java
+++ src/main/java/com/multi/fourtunes/model/biz/PlaylistBizImple.java
@@ -1,13 +1,32 @@
 package com.multi.fourtunes.model.biz;
 
+import com.multi.fourtunes.model.dao.SongDao;
 import com.multi.fourtunes.model.dto.SongDto;
 import com.multi.fourtunes.model.dto.UserDto;
+import com.multi.fourtunes.model.jpa.entity.SongEntity;
+import com.multi.fourtunes.model.jpa.repository.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PlaylistBizImple implements PlaylistBiz{
+
+    @Autowired
+    SongDao songDao;
+
+    @Autowired
+    SongRepository songRepository;
+
     @Override
     public String insertPlaylist(SongDto song, UserDto user) {
 
-
+        // 추가하려는 노래가 DB 에 존재하는지 확인
+        // SongDto selectSong = songDao.SelectSongById(song.getSongId());
+        try {
+            SongEntity selectSong = songRepository.findBySongId(song.getSongId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
