62186fa297362c41157cc4a8c79892e8158a24c6
J.M.Bae <bejm1997@gmail.com>
2023-06-21 22:34:36 +0900

    Update RoleManagerMapper.java
    USER 권한 불러오기

--- src/main/java/com/multi/fourtunes/model/mapper/RoleManagerMapper.java
+++ src/main/java/com/multi/fourtunes/model/mapper/RoleManagerMapper.java
@@ -2,9 +2,13 @@ package com.multi.fourtunes.model.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface RoleManagerMapper {
     @Insert(" INSERT INTO ROLE_MANAGE VALUES(2, #{userNo} )")
     void insertUserRole(int userNo);
+
+    @Select(" SELECT UR.ROLE_NAME FROM USER U JOIN ROLE_MANAGE RM ON U.USER_NO = RM.USER_NO JOIN USER_ROLE UR ON RM.ROLE_NO = UR.ROLE_NO WHERE U.USER_NO = #{userNo} ")
+    String getUserRoleByUserNo(int userNo);
 }
