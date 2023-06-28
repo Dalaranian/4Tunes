b34a38b6e4065a2d765e8d442090a4b604e4b095
Yujihoon1 <yjh150062@naver.com>
2023-06-28 10:07:33 +0900

    html
    html 정리

---
+++ src/main/resources/static/js/detail_delete.js
@@ -0,0 +1,9 @@
+document.getElementById("delete-btn").addEventListener("click",
+					function(event) {
+						event.preventDefault(); // 폼 제출 이벤트 중지
+
+						if (confirm("정말로 삭제하시겠습니까?")) {
+							// 확인 버튼을 눌렀을 때 폼을 제출
+							document.getElementById("delete-form").submit();
+						}
+					});	
