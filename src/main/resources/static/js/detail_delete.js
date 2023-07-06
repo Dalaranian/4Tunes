4a834974811e2b0f4c300ab027e7642e4738dda7
Yunji97 <simyg97@naver.com>
2023-07-06 15:51:25 +0900

    영어 댓글 안넘어가도록 수정
    댓글 삭제 alert창 랜덤으로 뜨는 문제

--- src/main/resources/static/js/detail_delete.js
+++ src/main/resources/static/js/detail_delete.js
@@ -1,3 +1,4 @@
+// 게시글 삭제 버튼 클릭 시 확인 창 표시
 document.getElementById("delete-btn").addEventListener("click",
 					function(event) {
 						event.preventDefault(); // 폼 제출 이벤트 중지
@@ -7,3 +8,24 @@ document.getElementById("delete-btn").addEventListener("click",
 							document.getElementById("delete-form").submit();
 						}
 					});	
+					
+	    // 댓글 삭제 버튼 클릭 시 확인 창 표시		
+	    const deleteButtons = document.querySelectorAll('.comment-delete');
+
+	    deleteButtons.forEach(button => {
+	        button.addEventListener('click', function(event) {
+	            var confirmDelete = confirm("정말로 삭제하시겠습니까?");
+	            if (!confirmDelete) {
+	                event.preventDefault(); // 삭제를 취소하고 페이지 이동을 막음
+	            }
+	        });
+	    });	
+					
+	
+    
+    
+      
+    
+    
+      
+    
