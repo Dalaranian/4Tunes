3d9bd044f33465ec53d1ddb67bcdbd67e6822369
JMBAE <bejm1997@gmail.com>
2023-07-06 16:42:38 +0900

    댓글 삭제 시 경고창 띄우기
    고쳤읍니다.
    
    Co-Authored-By: Yunji97 <128116526+Yunji97@users.noreply.github.com>

--- src/main/resources/static/js/detail_delete.js
+++ src/main/resources/static/js/detail_delete.js
@@ -1,4 +1,4 @@
-// 게시글 삭제 버튼 클릭 시 확인 창 표시
+$(function() {
 document.getElementById("delete-btn").addEventListener("click",
 					function(event) {
 						event.preventDefault(); // 폼 제출 이벤트 중지
@@ -7,20 +7,15 @@ document.getElementById("delete-btn").addEventListener("click",
 							// 확인 버튼을 눌렀을 때 폼을 제출
 							document.getElementById("delete-form").submit();
 						}
-					});	
-					
-	    // 댓글 삭제 버튼 클릭 시 확인 창 표시		
-	    const deleteButtons = document.querySelectorAll('.comment-delete');
+					});
+});
 
-	    deleteButtons.forEach(button => {
-	        button.addEventListener('click', function(event) {
-	            var confirmDelete = confirm("정말로 삭제하시겠습니까?");
-	            if (!confirmDelete) {
-	                event.preventDefault(); // 삭제를 취소하고 페이지 이동을 막음
-	            }
-	        });
-	    });	
-					
+function deleteComment(commentNo){
+    if(confirm("정말로 삭제하시겠습니까?")){
+        location.href="/community/delete/comment/"+commentNo;
+    }
+}
+
 	
     
     
