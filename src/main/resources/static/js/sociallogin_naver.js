70c0c90ecdcc863bfbeab6482479b92426c19c44
Yujihoon1 <yjh150062@naver.com>
2023-07-10 15:43:06 +0900

    naverlogin
    naver social login

--- src/main/resources/static/js/sociallogin_naver.js
+++ src/main/resources/static/js/sociallogin_naver.js
@@ -1,9 +1,9 @@
 var naver_id_login = new naver_id_login(
   "_Bz0QIIPqcH1PJ3NCi5I",
-  "http://localhost:8787/login/callback"
+  "http://ec2-52-78-2-172.ap-northeast-2.compute.amazonaws.com:8787/login/callback"
 );
 var state = naver_id_login.getUniqState();
 naver_id_login.setButton("green", 1, 72);
-naver_id_login.setDomain("http://localhost:8787");
+naver_id_login.setDomain("http://ec2-52-78-2-172.ap-northeast-2.compute.amazonaws.com:8787/");
 naver_id_login.setState(state);
 naver_id_login.init_naver_id_login();
