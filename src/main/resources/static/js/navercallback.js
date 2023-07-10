707806ce8c7d5946e1715103b1d18e4db5440164
JMBAE <bejm1997@gmail.com>
2023-07-10 16:39:27 +0900

    도메인 오류 수정

--- src/main/resources/static/js/navercallback.js
+++ src/main/resources/static/js/navercallback.js
@@ -1,10 +1,10 @@
 var naver_id_login = new naver_id_login(
   "_Bz0QIIPqcH1PJ3NCi5I",
-  "http://ec2-52-78-2-172.ap-northeast-2.compute.amazonaws.com/login/join"
+  "http://ec2-52-78-2-172.ap-northeast-2.compute.amazonaws.com:8787/login/join"
 );
 var state = naver_id_login.getUniqState();
 naver_id_login.setButton("green", 1, 0);
-naver_id_login.setDomain("http://ec2-52-78-2-172.ap-northeast-2.compute.amazonaws.com");
+naver_id_login.setDomain("http://ec2-52-78-2-172.ap-northeast-2.compute.amazonaws.com:8787");
 naver_id_login.setState(state);
 naver_id_login.setPopup();
 naver_id_login.init_naver_id_login();
