42e59533cb947baea5129632c6ab299178de37d3
J.M.Bae <bejm1997@gmail.com>
2023-07-11 09:14:51 +0900

    Update ChatbotController.java
    log 적용

--- src/main/java/com/multi/fourtunes/model/controller/ChatbotController.java
+++ src/main/java/com/multi/fourtunes/model/controller/ChatbotController.java
@@ -32,7 +32,7 @@ public class ChatbotController {
 
 			ObjectMapper mapper = new ObjectMapper();
 			String jsonInString = mapper.writeValueAsString(params);
-			System.out.println(jsonInString);
+			log.info(jsonInString);
 
 			// 발화 들어온거 확인하기 
 			// messageText = "발화내용" 
