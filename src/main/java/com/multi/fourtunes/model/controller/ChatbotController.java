3b521b855d41b209d070856912b43a18b01f141e
J.M.Bae <bejm1997@gmail.com>
2023-07-11 09:17:54 +0900

    Update ChatbotController.java
    빈 등록 중복 어노테이션 제거

--- src/main/java/com/multi/fourtunes/model/controller/ChatbotController.java
+++ src/main/java/com/multi/fourtunes/model/controller/ChatbotController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Component
+@Slf4j
 @RestController
 public class ChatbotController {
 
